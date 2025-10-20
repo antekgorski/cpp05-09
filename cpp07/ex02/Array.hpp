de676a952414de9418ccfa7121fb391506fef9bb
Antoni Gorski <agorski@c1r8s4.42warsaw.pl>
2025-10-20 13:04:31 +0200

    cpp07/ex03 checked

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: agorski <agorski@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 12:10:27 by agorski           #+#    #+#             */
-/*   Updated: 2025/10/20 12:28:30 by agorski          ###   ########.fr       */
+/*   Updated: 2025/10/20 13:03:06 by agorski          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,72 +16,64 @@
 #include <stdexcept> // for std::out_of_range
 #include <cstdlib> // for std::rand, std::srand
 #include <ctime> // for std::time
-
 template <typename T>
 class Array {
-private:
-    T* _arr;
-    unsigned int _size;
+    private:
+        T* _arr;
+        unsigned int _size;
+    
+    public:
+        Array() : _arr(NULL), _size(0) {}
 
-public:
-    // Konstruktor domyślny - pusta tablica
-    Array() : _arr(NULL), _size(0) {}
-    
-    // Konstruktor z rozmiarem
-    Array(unsigned int n) : _arr(NULL), _size(n) {
-        if (n > 0) {
-            _arr = new T[n];
-            for (unsigned int i = 0; i < n; i++) {
-                _arr[i] = T();
-            }
-        }
-    }
-    
-    // Konstruktor kopiujący
-    Array(const Array& other) : _arr(NULL), _size(0) {
-        *this = other;
-    }
-    
-    // Destruktor
-    ~Array() {
-        delete[] _arr;
-    }
-    
-    // Operator przypisania
-    Array& operator=(const Array& other) {
-        if (this != &other) {
-            delete[] _arr;
-            _size = other._size;
-            if (_size > 0) {
-                _arr = new T[_size];
-                for (unsigned int i = 0; i < _size; i++) {
-                    _arr[i] = other._arr[i];
+        Array(unsigned int n) : _arr(NULL), _size(n) {
+            if (n > 0) {
+                _arr = new T[n];
+                for (unsigned int i = 0; i < n; i++) {
+                    _arr[i] = T();
                 }
-            } else {
-                _arr = NULL;
             }
         }
-        return *this;
-    }
-    
-    // Operator [] - non-const
-    T& operator[](unsigned int index) {
-        if (index >= _size)
-            throw std::out_of_range("Index out of bounds");
-        return _arr[index];
-    }
-    
-    // Operator [] - const
-    const T& operator[](unsigned int index) const {
-        if (index >= _size)
-            throw std::out_of_range("Index out of bounds");
-        return _arr[index];
-    }
-    
-    // size()
-    unsigned int size() const {
-        return _size;
-    }
+
+        Array(const Array& other) : _arr(NULL), _size(0) {
+            *this = other;
+        }
+
+        ~Array() {
+            delete[] _arr;
+        }
+
+        Array& operator=(const Array& other) {
+            if (this != &other) {
+                delete[] _arr;
+                _size = other._size;
+                if(_size > 0) {
+                    _arr = new T[_size];
+                    for (unsigned int i = 0; i < _size; i++) {
+                        _arr[i] = other._arr[i];
+                    }
+                }
+                else {
+                    _arr = NULL;
+                }
+            }
+            return *this;
+        }
+        
+        T& operator[](unsigned int index) {
+            if (index >= _size)
+                throw std::out_of_range("Index out of bounds");
+            return _arr[index];
+        }
+
+        const T& operator[](unsigned int index) const {
+            if (index >= _size)
+                throw std::out_of_range("Index out of bounds");
+            return _arr[index];
+        }
+
+        unsigned int size() const {
+            return _size;
+        }
 };
 
 #endif
