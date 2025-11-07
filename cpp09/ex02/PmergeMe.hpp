1e383ee674c3a9316e779a51191b42a11018649e
Antoni Gorski <agorski@c1r4s4.42warsaw.pl>
2025-11-07 18:16:29 +0100

    olmost done only fibonaci to implement

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: agorski <agorski@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 14:54:06 by agorski           #+#    #+#             */
-/*   Updated: 2025/11/05 14:25:54 by agorski          ###   ########.fr       */
+/*   Updated: 2025/11/07 16:57:37 by agorski          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,146 @@
 #define PmergeMe_HPP
 
 #include <iostream>
-#include <string>
-#include <vector>
-#include <deque>
-#include <algorithm>
 #include <cerrno>
 #include <cstdlib>
 #include <climits>
 #include <stdexcept>
+
+template <typename container>
 class PmergeMe
 {
     private:
-    
-        std::deque<int> _deque;
-        std::vector<int> _vector;
+        container _data;
+        container _sortedData;
 
         PmergeMe();
         PmergeMe(const PmergeMe& other);
         PmergeMe& operator=(const PmergeMe& other);
-        ~PmergeMe();
         
-    public:
-
-        template <typename container>
-        container FordJohnsonSort(container& input);
-
+        public:
+        
         PmergeMe(char **argv);
+        ~PmergeMe();
+        container FordJohnsonSort(const container& input);
         void parseInput(char **argv);
-        
+        void printData(const container& data) const;
+        container getData() const;
+        container getSortedData() const;
+        typename container::size_type getSize() const;
+};
+
+template <typename container>
+PmergeMe<container>::PmergeMe(char **argv)
+{
+    parseInput(argv);
+};
+
+template <typename container>
+PmergeMe<container>::~PmergeMe()
+{
+};
+
+template <typename container>
+typename container::size_type PmergeMe<container>::getSize() const
+{
+    return _data.size();
+};
+
+template <typename container>
+container PmergeMe<container>::getData() const
+{
+    return _data;
+};
+
+template <typename container>
+container PmergeMe<container>::getSortedData() const
+{
+    return _sortedData;
+};
+
+template <typename container>
+void PmergeMe<container>::printData(const container& data) const
+{
+for (typename container::const_iterator it = data.begin(); it != data.end(); ++it)
+    std::cout << *it << " ";
+};
+
+template <typename container>
+container PmergeMe<container>::FordJohnsonSort(const container& input)
+{
+// 1. Separete into pairs and sort each pair.
+if (input.size() <= 1)
+        return input;
+
+container small, large, mainChain;
+
+typename container::size_type i = 0;
+while (i + 1 < input.size())
+{
+    if (input[i] < input[i + 1])
+    {
+        small.push_back(input[i]);
+        large.push_back(input[i + 1]);
+    }
+    else
+    {
+        small.push_back(input[i + 1]);
+        large.push_back(input[i]);
+    }
+    i += 2;
+}
+
+// 2. Handle odd element
+if (input.size() % 2 != 0)
+{
+    small.push_back(input.back());
+}
+
+// 2. Sort "large" elements recursively
+large = FordJohnsonSort(large);
+
+// 3. Merge "small" and "large"
+mainChain = large;
+
+
+
+return mainChain;
+};
+
+template <typename container>
+void PmergeMe<container>::parseInput(char **argv)
+{
+    try
+{
+    for (int i = 1; argv[i]; ++i)
+    {
+        char *endptr = NULL;
+        errno = 0;
+
+        long val = std::strtol(argv[i], &endptr, 10);
+
+        // 1. Nic nie zostało sparsowane → nie jest liczbą
+        if (endptr == argv[i])
+            throw std::runtime_error("Error");
+
+        // 2. Są śmieci po liczbie → np. "12abc"
+        if (*endptr != '\0')
+            throw std::runtime_error("Error");
+
+        // 3. Overflow/underflow
+        if (errno == ERANGE || val > INT_MAX || val < 0)
+            throw std::runtime_error("Error");
+
+        int v = static_cast<int>(val);
+
+        _data.push_back(v);
+    }
+}
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        exit(1);
+    }
 };
 
 #endif
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,7 +6,59 @@
 /*   By: agorski <agorski@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 14:54:09 by agorski           #+#    #+#             */
-/*   Updated: 2025/10/25 14:54:10 by agorski          ###   ########.fr       */
+/*   Updated: 2025/11/07 18:07:01 by agorski          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PmergeMe.hpp"
+#include <vector>
+#include <deque>
+#include <ctime>
+#include <iomanip>
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        std::cerr << "Error: Not enough arguments" << std::endl;
+        return 1;
+    }
+    
+     
+    clock_t vStart, vEnd;
+    vStart = clock();
+    PmergeMe< std::vector<int> > vectorSorter(argv);
+    std::vector<int> sortedVector = vectorSorter.FordJohnsonSort(vectorSorter.getData());
+    vEnd = clock();
+    double vDuration = static_cast<double>(vEnd - vStart) / CLOCKS_PER_SEC * 1000000; // microseconds
+    
+    clock_t dStart, dEnd;
+    dStart = clock();
+    PmergeMe< std::deque<int> > dequeSorter(argv);
+    std::deque<int> sortedDeque = dequeSorter.FordJohnsonSort(dequeSorter.getData());
+    dEnd = clock();
+    double dDuration = static_cast<double>(dEnd - dStart) / CLOCKS_PER_SEC * 1000000;
+    
+    std::cout << "Before:" << '\t';
+    vectorSorter.printData(vectorSorter.getData());
+    //dequeSorter.printData(dequeSorter.getData());
+    std::cout << std::endl;
+    std::cout << "After:" << '\t';
+    vectorSorter.printData(sortedVector);
+    //dequeSorter.printData(sortedDeque);
+
+    std::cout << std::endl;
+    std::cout << "Time to process a range of " 
+                << vectorSorter.getData().size() 
+                << " elements with std::vector:" << "\t"
+                << std::fixed << std::setprecision(5) 
+                << vDuration << " us" << std::endl;
+
+    std::cout << "Time to process a range of " 
+            << vectorSorter.getData().size() 
+                << " elements with std::deque:" << "\t"
+                << std::fixed << std::setprecision(5) 
+                << dDuration << " us" << std::endl;
+
+    return 0;
+}   
